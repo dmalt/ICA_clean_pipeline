e73f648602dfac509f6a0b08edfea01da3014664
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-03-29 13:26:03 +0300

    Updated reportGen.py to a working version and cleaned ica_preproc.py
    	modified:   ica_preproc.py
    	modified:   reportGen.py

--- ica_preproc.py
+++ ica_preproc.py
@@ -1,15 +1,14 @@
 def preprocess_ICA_fif_to_ts(fif_file, ECG_ch_name, EoG_ch_name, l_freq, h_freq):
     # ------------------------ Import stuff ------------------------ #
     import os
-    import numpy as np
     import mne
     from mne.io import Raw	
     from mne.preprocessing import ICA, read_ica
     from mne.preprocessing import create_ecg_epochs, create_eog_epochs
-    from mne.report import Report
     from nipype.utils.filemanip import split_filename as split_f
-    
-    report = Report()
+    from reportGen import generateReport
+
+
 
     subj_path,basename,ext = split_f(fif_file)
     ##################### Delete later #####################
@@ -83,7 +82,6 @@ def preprocess_ICA_fif_to_ts(fif_file, ECG_ch_name, EoG_ch_name, l_freq, h_freq)
     ### 2) identify bad components by analyzing latent sources.
     # generate ECG epochs use detection via phase statistics
     
-    n_max_eog = 4
     
     # check if ECG_ch_name is in the raw channels
     if ECG_ch_name in raw.info['ch_names']:
@@ -93,71 +91,29 @@ def preprocess_ICA_fif_to_ts(fif_file, ECG_ch_name, EoG_ch_name, l_freq, h_freq)
         ecg_epochs = create_ecg_epochs(raw, tmin=-.5, tmax=.5, picks=select_sensors)
     ### ICA for ECG artifact 
     # threshold=0.25 come defualt
-    ecg_inds, scores = ica.find_bads_ecg(ecg_epochs, method='ctps', threshold=0.25)
+    ecg_inds, ecg_scores = ica.find_bads_ecg(ecg_epochs, method='ctps', threshold=0.25)
     # if len(ecg_inds) > 0:
     ecg_evoked = ecg_epochs.average()    
     ecg_epochs = None    # ecg_epochs use too much memory
-    
-    # --------------------- Generate report for ECG ---------------------------------------- #
-    fig1 = ica.plot_scores(scores, exclude=ecg_inds, title=ICA_title % 'ecg', show=is_show)
-    show_picks = np.abs(scores).argsort()[::-1][:5] # Pick the five largest scores and plot them
-
-    # Plot estimated latent sources given the unmixing matrix.
-
-    # topoplot of unmixing matrix columns
-    fig3 = ica.plot_components(show_picks, title=ICA_title % 'ecg', colorbar=True, show=is_show)
-
     n_max_ecg = 3
     ecg_inds = ecg_inds[:n_max_ecg]
     ica.exclude += ecg_inds
-
-    fig4 = ica.plot_sources(ecg_evoked, exclude=ecg_inds, show=is_show)  # plot ECG sources + selection
-    fig = [fig1, fig3, fig4]
-    report.add_figs_to_section(fig, captions=['Scores of ICs related to ECG',
-                                              'TopoMap of ICs (ECG)', 
-                                              'Time-locked ECG sources'], section = 'ICA - ECG for ' + subj_name)  
-    # ----------------------------------- end generate report for ECG ------------------------------- #
-
-    
-    eog_inds, scores = ica.find_bads_eog(raw, ch_name=EoG_ch_name)
-
+    n_max_eog = 4
+    eog_inds, eog_scores = ica.find_bads_eog(raw, ch_name=EoG_ch_name)
+    eog_inds = eog_inds[:n_max_eog]
     # ------------------------------------------------------ # 
     # This is necessary. Otherwise line 
     # will through an exception
-    eog_inds = eog_inds[:n_max_eog]
     if not eog_inds:
         eog_inds = None
     # ------------------------------------------------------ #
+    generateReport(raw=raw, ica=ica, subj_name=subj_name, subj_path=subj_path, basename=basename,
+                   ecg_evoked=ecg_evoked, ecg_scores=ecg_scores, ecg_inds=ecg_inds, ECG_ch_name=ECG_ch_name,
+                   eog_evoked=None, eog_scores=eog_scores, eog_inds=eog_inds, EoG_ch_name=EoG_ch_name)
 
-    fig6 = ica.plot_scores(scores, exclude=eog_inds, title=ICA_title % 'eog', show=is_show)
-    report.add_figs_to_section(fig6, captions=['Scores of ICs related to EOG'], 
-                       section = 'ICA - EOG')
-                       
-    # check how many EoG ch we have
-    # --------------------------------- Generate report for EoG --------------------------------------------- #
-    rs = np.shape(scores)
-    if len(rs)>1:
-        rr = rs[0]
-        show_picks = [np.abs(scores[i][:]).argsort()[::-1][:5] for i in range(rr)]
-        for i in range(rr):
-            fig8 = ica.plot_components(show_picks[i][:], title=ICA_title % 'eog',
-                                       colorbar=True, show=is_show) # ICA nel tempo
-            fig = [fig8]
-            report.add_figs_to_section(fig, captions=['Scores of ICs related to EOG'],
-                                        section = 'ICA - EOG')    
-    else:
-        show_picks = np.abs(scores).argsort()[::-1][:5]                                  
-        fig8 = ica.plot_components(show_picks, title=ICA_title % 'eog', colorbar=True, show=is_show) 
-        fig = [fig8]            
-        report.add_figs_to_section(fig, captions=['TopoMap of ICs (EOG)',],
-                                        section = 'ICA - EOG') 
-       # -------------------- end generate report for EoG -------------------------------------------------------- #
+
     sfreq = raw.info['sfreq']
     raw = None # To save memory
-
-    report_filename = os.path.join(subj_path,basename + "-report.html")
-    print '******* ' + report_filename
-    report.save(report_filename, open_browser=False, overwrite=True)
             
     ### save ICA solution  
     print ica_filename
